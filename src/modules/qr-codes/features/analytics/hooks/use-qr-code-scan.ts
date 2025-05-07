2daa0170c9914e077b0f112ba3fd845b7b8782a7
Alekseypavlov14 <aleshapavlov9@gmail.com>
2025-05-07 19:32:25 +0300

    added locker to prevent mulitple scans

--- src/modules/qr-codes/features/analytics/hooks/use-qr-code-scan.ts
+++ src/modules/qr-codes/features/analytics/hooks/use-qr-code-scan.ts
@@ -1,13 +1,19 @@
-import { updateStatusSelector, useRedirectStatusStore } from '../stores/redirect-status.store'
 import { redirectStatusFail, redirectStatusPending, redirectStatusSuccess } from '../constants'
+import { updateStatusSelector, useRedirectStatusStore } from '../stores/redirect-status.store'
 import { mapDateToISOString } from '@/shared/utils/datetime'
 import { qrCodeAnalyticsAPI } from '../qr-code-analytics.api'
+import { useRef } from 'react'
 
 export function useQRCodeScan() {
+  const isExecutedRef = useRef<boolean>(false)
   const updateStatus = useRedirectStatusStore(updateStatusSelector)
 
   function scanQRCode(hash: string) {
     const date = mapDateToISOString(new Date())
+
+    // locker
+    if (isExecutedRef.current) return
+    isExecutedRef.current = true
 
     // set pending status
     updateStatus(redirectStatusPending)
