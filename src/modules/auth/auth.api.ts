f3e911da2a5b04bafc113bc9f0d341e6f8466fe8
Alekseypavlov14 <aleshapavlov9@gmail.com>
2025-05-07 19:18:47 +0300

    added fix with correct handling auth methods

--- src/modules/auth/auth.api.ts
+++ src/modules/auth/auth.api.ts
@@ -3,13 +3,13 @@ import { httpClient } from '@/shared/utils/http'
 import { saveToken } from './middlewares/save-token'
 
 export class AuthAPI {
-  async signIn(credentials: Credentials): Promise<void> {
-    httpClient.post<Credentials, Response>(`/auth/sign-in`, credentials, {}, { parse: false })
+  async signIn(credentials: Credentials): Promise<Response> {
+    return await httpClient.post<Credentials, Response>(`/auth/sign-in`, credentials, {}, { parse: false })
       .then(saveToken)
   }
 
-  async signUp(credentials: Credentials): Promise<void> {
-    httpClient.post<Credentials, Response>(`/auth/sign-up`, credentials, {}, { parse: false })
+  async signUp(credentials: Credentials): Promise<Response> {
+    return await httpClient.post<Credentials, Response>(`/auth/sign-up`, credentials, {}, { parse: false })
       .then(saveToken)
   }
 }
--- src/modules/qr-codes/features/feed/hooks/use-qr-codes.ts
+++ src/modules/qr-codes/features/feed/hooks/use-qr-codes.ts
@@ -1,16 +1,21 @@
+import { useAppExceptionHandler } from '@/app/exceptions'
 import { useEffect, useState } from 'react'
 import { qrCodesFeedAPI } from '../qr-codes-feed.api'
+import { alwaysHandler } from '@oleksii-pavlov/error-handling'
 import { QRCodeEntity } from '@/modules/qr-codes'
 
 export function useQRCodes() {
   const [qrCodes, setQRCodes] = useState<QRCodeEntity[]>([])
+  const appExceptionHandler = useAppExceptionHandler()
 
   useEffect(() => {
     qrCodesFeedAPI.getAll()
       .then(qrCodes => {
         setQRCodes(qrCodes)
       })
-      .catch(() => setQRCodes([]))
+      .catch(appExceptionHandler({
+        [alwaysHandler]: () => setQRCodes([])
+      }))
   }, [])
 
   return qrCodes
