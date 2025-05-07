99e45ebe943c8077ae80ee4cf916e23fbad0bcc4
Alekseypavlov14 <aleshapavlov9@gmail.com>
2025-05-07 20:27:44 +0300

    added logo navigation

--- src/widgets/DashboardHeader/DashboardHeader.tsx
+++ src/widgets/DashboardHeader/DashboardHeader.tsx
@@ -1,11 +1,14 @@
+import { useNavigation } from '@/app/navigation'
 import { Header } from '@/shared/components/Header'
 import { Logo } from '@/shared/components/Logo'
 import styles from './DashboardHeader.module.css'
 
 export function DashboardHeader() {
+  const { navigateHomePage } = useNavigation()
+
   return (
     <Header className={styles.DashboardHeader}>
-      <Logo />
+      <Logo onClick={navigateHomePage} />
     </Header>
   )
 }
