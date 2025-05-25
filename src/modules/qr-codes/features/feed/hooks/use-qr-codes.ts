1f703d062b0909cee54748cc5d56beca4ea1e1c7
Alekseypavlov14 <aleshapavlov9@gmail.com>
2025-05-25 13:47:00 +0300

    added loader to home screen to improve UX

--- src/modules/qr-codes/features/feed/hooks/use-qr-codes.ts
+++ src/modules/qr-codes/features/feed/hooks/use-qr-codes.ts
@@ -1,23 +1,35 @@
 import { useAppExceptionHandler } from '@/app/exceptions'
 import { useEffect, useState } from 'react'
 import { sortQRCodesByDate } from '../utils/sort-qr-codes-by-date'
+import { useNotifications } from '@/app/notifications'
 import { qrCodesFeedAPI } from '../qr-codes-feed.api'
 import { alwaysHandler } from '@oleksii-pavlov/error-handling'
 import { QRCodeEntity } from '@/modules/qr-codes'
 
 export function useQRCodes() {
   const [qrCodes, setQRCodes] = useState<QRCodeEntity[]>([])
+  const [isLoading, setLoading] = useState(false)
+
   const appExceptionHandler = useAppExceptionHandler()
+  const { failure } = useNotifications()
 
   useEffect(() => {
+    setLoading(true)
+
     qrCodesFeedAPI.getAll()
       .then(qrCodes => {
         setQRCodes(sortQRCodesByDate(qrCodes))
       })
       .catch(appExceptionHandler({
-        [alwaysHandler]: () => setQRCodes([])
+        [alwaysHandler]: () => {
+          failure('Error occurred. Try again')
+          setQRCodes([])
+        }
       }))
+      .finally(() => {
+        setLoading(false)
+      })
   }, [])
 
-  return qrCodes
+  return { qrCodes, isLoading }
 }
--- src/modules/qr-codes/features/feed/widgets/QRCodesList/QRCodesList.tsx
+++ src/modules/qr-codes/features/feed/widgets/QRCodesList/QRCodesList.tsx
@@ -2,13 +2,20 @@ import { useNavigation } from '@/app/navigation'
 import { GridFallback } from '@/shared/components/GridFallback'
 import { QRCodeCard } from '../../components/QRCodeCard'
 import { useQRCodes } from '../../hooks/use-qr-codes'
+import { Center } from '@/shared/components/Center'
+import { Loader } from '@/shared/components/Loader'
 import { Grid } from '@/shared/components/Grid'
 import styles from './QRCodesList.module.css'
 
 export function QRCodesList() {
   const { navigateQRCodeAnalyticsPage } = useNavigation()
+  const { qrCodes, isLoading } = useQRCodes()
 
-  const qrCodes = useQRCodes()
+  if (isLoading) return (
+    <Center>
+      <Loader />
+    </Center>
+  )
 
   return (
     <Grid 
